5a8da7519999aaf777216622577c048ed852b808
Utkirbek10 <olimovotkirbek49@gmail.com>
2022-07-28 14:06:02 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dvgr1%-+$-#srzm0&+o8-pbzyx^m@kql)wrxhpmgex3w-8(shf'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG',default = False)
 
 ALLOWED_HOSTS = []
 
@@ -52,6 +56,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount', 
     'dj_rest_auth',
     'dj_rest_auth.registration',
+    'drf_yasg', # new
 ]
 
 REST_FRAMEWORK = {
@@ -59,8 +64,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [ # new
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 3
--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,26 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.schemas import get_schema_view
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view as doc_schema_view
+from drf_yasg import openapi
+
+schema_view = doc_schema_view(
+    openapi.Info(
+        title='Online Shop',
+        default_version='v1',
+        description='Online shopping',
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email='olimovotkirbek49@gmail.com'),
+        license=openapi.License(name='Alimov Brand'),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls')),
-    path('',include('shop.urls')),
     path('orders/',include('orders.urls')),
     path('api/v1/',include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
@@ -19,6 +34,12 @@ urlpatterns = [
         description="A sample API for learning DRF",
         version="1.0.0"
     ), name='openapi-schema'),
+    path('swagger/',schema_view.with_ui(
+        'swagger',cache_timeout=0),name = 'schema_swagger_ui'),
+    path('redoc/',schema_view.with_ui(
+        'redoc',cache_timeout=0),name='schema_redoc'),
+    path('',include('shop.urls')),
+    
 ]
 
 if settings.DEBUG:
